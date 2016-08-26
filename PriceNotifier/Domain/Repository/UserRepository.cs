58daaf44656ed9c34b4eb30eb11ffaff607a9dbd
Stan <stanislav.soloview@gmail.com>
2016-08-26 18:28:29 +0300

    Added UserCtrl

---
+++ PriceNotifier/Domain/Repository/UserRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.EF;
+using Domain.Entities;
+
+namespace Domain.Repository
+{
+    public class UserRepository:IRepository<User>
+    {
+        private readonly UserContext _context;
+
+        public UserRepository(UserContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<User> Query()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> Get(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Create(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(User entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
