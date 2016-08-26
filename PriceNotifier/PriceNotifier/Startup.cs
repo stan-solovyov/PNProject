7efef9ea4c766c6e5ef1cfc0419acd37324226c4
Stan <stanislav.soloview@gmail.com>
2016-08-26 16:12:16 +0300

    Added Autofac DI container

--- PriceNotifier/PriceNotifier/Startup.cs
+++ PriceNotifier/PriceNotifier/Startup.cs
@@ -1,4 +1,11 @@
-﻿using System.Web.Http;
+﻿using System.Reflection;
+using System.Web.Http;
+using Autofac;
+using Autofac.Integration.WebApi;
+using BLL.ProductService;
+using Domain.EF;
+using Domain.Entities;
+using Domain.Repository;
 using Microsoft.Owin;
 using Owin;
 using PriceNotifier.DTO;
@@ -11,8 +18,21 @@ namespace PriceNotifier
     {
         public void Configuration(IAppBuilder app)
         {
+
+            var builder = new ContainerBuilder();
+
             HttpConfiguration config = new HttpConfiguration();
             WebApiConfig.Register(config);
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            builder.Register(context => new UserContext()).As<UserContext>().InstancePerRequest();
+            builder.RegisterType<ProductRepository>().As<IRepository<Product>>().InstancePerRequest();
+            builder.RegisterType<ProductService>().As<IService<Product>>().InstancePerRequest();
+
+            var container = builder.Build();
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+
+            app.UseAutofacMiddleware(container);
+            app.UseAutofacWebApi(config);
             app.UseWebApi(config);
             AutoMapperInitializer.Initialize();
         }
