fd8edf249dcb406e4420651e856674699a466caa
Stan <stanislav.soloview@gmail.com>
2016-08-26 12:12:15 +0300

    Remove some unused namespaces

--- PriceNotifier/PriceNotifier/Startup.cs
+++ PriceNotifier/PriceNotifier/Startup.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Threading.Tasks;
-using System.Web.Http;
+﻿using System.Web.Http;
 using Microsoft.Owin;
 using Owin;
 using PriceNotifier.DTO;
-using WebGrease.Configuration;
 
 [assembly: OwinStartup(typeof(PriceNotifier.Startup))]
 
