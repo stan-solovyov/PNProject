b88f80e0d9a51cfa8efaba8314491569360ec586
Stan <stanislav.soloview@gmail.com>
2016-08-29 18:48:13 +0300

    Added UserCtrl and Users view

---
+++ PriceNotifier/PriceNotifier/Scripts/app/Controllers/UserCtrl.js
@@ -0,0 +1,32 @@
+﻿app.controller('UserCtrl', ['$scope', 'userService', function ($scope, userService) {
+
+    var onError = function () {
+        $scope.error = "Couldn't get response from the server:(";
+    };
+
+    var onGetUsers = function (response) {
+        $scope.users = response.data;
+        if ($scope.users.length === 0 && response.status !== 204) {
+            $scope.Message = "You don't have any items in the list yet.";
+            $scope.demonstrate = true;
+        } else {
+            $scope.Message = null;
+            $scope.demonstrate = false;
+        }
+    };
+
+    var onUserDelete = function () {
+        userService.getUsers().then(onGetUsers, onError);
+    };
+
+    $scope.update = function (product) {
+        productService.updateItem(product).then(onUserDelete, onError);
+    };
+
+    $scope.remove = function (id) {
+        userService.removeUser(id).then(onUserDelete, onError);
+    };
+
+    userService.getUsers().then(onGetUsers, onError);
+}
+]);
---
+++ PriceNotifier/PriceNotifier/Scripts/app/Services/UserService.js
@@ -0,0 +1,39 @@
+﻿app.factory("userService", ['$http', 'toaster',
+    function ($http, toaster) {
+
+        var url = '/api/Users';
+
+        var getUsers = function () {
+            return $http.get(url)
+                .then(function (response) {
+                    return response;
+                });
+        }
+
+        var updateUser = function (user) {
+            return $http.put('/api/Users/',
+                JSON.stringify(user)
+            ).success(function () {
+                toaster.pop('success', "", "The user profile was successfully updated.");
+            });
+        };
+
+        var removeUser = function (id) {
+            return $http({
+                method: 'DELETE',
+                url: '/api/Users/' + id,
+                headers: {
+                    'Content-type': 'application/json'
+                }
+            })
+                .success(function () {
+                    toaster.pop('success', "Congrats!", "The user profile was removed successfully.");
+                });
+        };
+
+        return {
+            getUsers: getUsers,
+            updateUser: updateUser,
+            removeUser: removeUser
+        };
+    }]);
