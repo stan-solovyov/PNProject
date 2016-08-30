7f2b2c7c7ab090f7f0253c52bec09d75fa9f7231
Stan <stanislav.soloview@gmail.com>
2016-08-30 18:49:50 +0300

    Added underscore.js library

---
+++ PriceNotifier/PriceNotifier/Scripts/app/Services/PagerService.js
@@ -0,0 +1,53 @@
+﻿app.factory("pagerService",
+[ function() {
+
+        // service implementation
+        var getPager = function (totalItems, currentPage, pageSize) {
+            // default to first page
+            currentPage = currentPage || 1;
+
+            // default page size is 10
+            pageSize = pageSize || 10;
+
+            // calculate total pages
+            var totalPages = Math.ceil(totalItems / pageSize);
+
+            var startPage, endPage;
+            if (totalPages <= 10) {
+                // less than 10 total pages so show all
+                startPage = 1;
+                endPage = totalPages;
+            } else {
+                // more than 10 total pages so calculate start and end pages
+                if (currentPage <= 6) {
+                    startPage = 1;
+                    endPage = 10;
+                } else if (currentPage + 4 >= totalPages) {
+                    startPage = totalPages - 9;
+                    endPage = totalPages;
+                } else {
+                    startPage = currentPage - 5;
+                    endPage = currentPage + 4;
+                }
+            }
+
+            // create an array of pages to ng-repeat in the pager control
+            var pages = _.range(startPage, endPage + 1);
+
+            // return object with all pager properties required by the view
+            return {
+                totalItems: totalItems,
+                currentPage: currentPage,
+                pageSize: pageSize,
+                totalPages: totalPages,
+                startPage: startPage,
+                endPage: endPage,
+                pages: pages
+            };
+        };
+
+    return {
+        getPager: getPager
+    };
+    }
+]);
