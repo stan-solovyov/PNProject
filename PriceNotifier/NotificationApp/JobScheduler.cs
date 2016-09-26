107cf52ef81bf2262616a1c91aab87ce16a7b2ad
Stan <stanislav.soloview@gmail.com>
2016-09-26 15:59:27 +0300

    Quartz implemented

---
+++ PriceNotifier/NotificationApp/JobScheduler.cs
@@ -0,0 +1,27 @@
+﻿using Quartz;
+using Quartz.Impl;
+
+namespace NotificationApp
+{
+    public class JobScheduler
+    {
+        public static void Start()
+        {
+            IScheduler scheduler = StdSchedulerFactory.GetDefaultScheduler();
+            scheduler.Start();
+
+            IJobDetail job = JobBuilder.Create<PriceUpdate>().Build();
+
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithDailyTimeIntervalSchedule
+                  (s =>
+                     s.WithIntervalInHours(2)
+                    .OnEveryDay()
+                    .StartingDailyAt(TimeOfDay.HourAndMinuteOfDay(10, 0))
+                  )
+                .Build();
+
+            scheduler.ScheduleJob(job, trigger);
+        }
+    }
+}
