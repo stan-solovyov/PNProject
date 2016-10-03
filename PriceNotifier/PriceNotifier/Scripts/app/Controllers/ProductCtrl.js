767b10cadfaa53c64a9ec006351dcffdfd8ff975
Stan <stanislav.soloview@gmail.com>
2016-10-03 18:48:16 +0300

    Added a flag to ProductCtrl.js

--- PriceNotifier/PriceNotifier/Scripts/app/Controllers/ProductCtrl.js
+++ PriceNotifier/PriceNotifier/Scripts/app/Controllers/ProductCtrl.js
@@ -50,22 +50,23 @@
         if (priceChanges.length === 0) {
             $scope.Note = "You don't have any history for this item.";
             $scope.show = true;
+            $scope.showChart = false;
         } else {
             $scope.Note = null;
             $scope.show = false;
+            $scope.showChart = true;
             $scope.priceChanges = priceChanges;
             var pricesForChart = [];
             var dataChart = [];
             angular.forEach(priceChanges,
-            function (p) {
-                //pricesForChart.push(p.NewPrice);
-                var date = new Date(p.Date);
-                datesForChart.push(date);
-                pricesForChart.push(p.NewPrice);
-            });
+                function (p) {
+                    var date = moment(p.Date);
+                    datesForChart.push(date);
+                    pricesForChart.push(p.NewPrice);
+                });
 
-            for (i = 0; i < pricesForChart.length; i++) {
-                var a =  {
+            for (var i = 0; i < pricesForChart.length; i++) {
+                var a = {
                     x: datesForChart[i],
                     y: pricesForChart[i]
                 };
@@ -73,67 +74,68 @@
             }
 
             $scope.data = [dataChart];
-        }
-
-        $scope.colors = ['#45b7cd', '#ff6384', '#ff8e72'];
-        $scope.series = ['Price change, BYN'];
-        $scope.labels = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-
-        $scope.datasetOverride = [
-          {
-              borderWidth: 3,
-              hoverBackgroundColor: "rgba(255,99,132,0.4)",
-              hoverBorderColor: "rgba(255,99,132,1)",
-              type: 'line'
-          }
-        ];
 
 
-        $scope.options = {
-            scales: {
-                xAxes: [{
-                    type: 'time',
-                    time: {
-                        //unit:'month',
-                        displayFormats: {
-                            quarter: 'MMM YYYY'
+            $scope.colors = ['#45b7cd', '#ff6384', '#ff8e72'];
+            $scope.series = ['Price change, BYN'];
+            $scope.labels = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+            $scope.datasetOverride = [
+                {
+                    borderWidth: 3,
+                    hoverBackgroundColor: "rgba(255,99,132,0.4)",
+                    hoverBorderColor: "rgba(255,99,132,1)",
+                    type: 'line'
+                }
+            ];
+
+            $scope.options = {
+                scales: {
+                    xAxes: [
+                        {
+                            type: 'time',
+                            time: {
+                                //unit:'hour',
+                                displayFormats: {
+                                    month: 'MMM D, hA'
+                                }
+                            },
+                            position: 'bottom'
                         }
-                    },
-                    position: 'bottom'
-                }]
-            }
+                    ]
+                }
+            };
+        };
+        $scope.ok = function () {
+            $uibModalInstance.close();
+        };
     };
 
-    $scope.ok = function () {
-        $uibModalInstance.close();
+    $scope.message = "There are no items in track list.";
+    var onUserProducts = function (response) {
+        $scope.dbproducts = response.data;
+        if ($scope.dbproducts.length === 0 && response.status !== 204) {
+            $scope.Message = "You don't have any items in the list yet.";
+            $scope.demonstrate = true;
+        } else {
+            $scope.Message = null;
+            $scope.demonstrate = false;
+        }
     };
-};
 
-$scope.message = "There are no items in track list.";
-var onUserProducts = function (response) {
-    $scope.dbproducts = response.data;
-    if ($scope.dbproducts.length === 0 && response.status !== 204) {
-        $scope.Message = "You don't have any items in the list yet.";
-        $scope.demonstrate = true;
-    } else {
-        $scope.Message = null;
-        $scope.demonstrate = false;
-    }
-};
+    var onUserDelete = function () {
+        productService.getProducts().then(onUserProducts, onError);
+    };
 
-var onUserDelete = function () {
+    $scope.update = function (product) {
+        productService.updateItem(product).then(onUserDelete, onError);
+    };
+
+    $scope.remove = function (product) {
+        productService.removeItem(product).then(onUserDelete, onError);
+    };
+
     productService.getProducts().then(onUserProducts, onError);
-};
-
-$scope.update = function (product) {
-    productService.updateItem(product).then(onUserDelete, onError);
-};
-
-$scope.remove = function (product) {
-    productService.removeItem(product).then(onUserDelete, onError);
-};
-
-productService.getProducts().then(onUserProducts, onError);
 }
 ]);
 
